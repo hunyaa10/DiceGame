3b679327e20681d8662d69a299a696a9a18594a9
hunyaa10 <161268128+hunyaa10@users.noreply.github.com>
2024-08-24 18:22:03 +0900

    js 수정

--- script.js
+++ script.js
@@ -18,54 +18,106 @@ let totalScore = 0;
 let isDisabled = true;
 let i = 0;
 
-const resetDice = () => {
-  diceNumber[i].innerText = `주사위 숫자 : 0`;
-  diceBuild[i].innerText = `주사위 합 : 0`;
-  num = 0;
-  diceSum = 0;
-};
+const firstPlayer = () => {
+  i = 0;
 
-const ariaDisabled = () => {
-  const disabledStatus = (element, status) => {
-    if (status) {
-      element.setAttribute("aria-disabled", "false");
-    } else {
-      element.setAttribute("aria-disabled", "true");
-    }
+  const resetDice = () => {
+    diceNumber[i].innerText = `주사위 숫자 : 0`;
+    diceBuild[i].innerText = `주사위 합 : 0`;
+    num = 0;
+    diceSum = 0;
   };
 
-  isDisabled = !isDisabled;
-  disabledStatus(player[0], isDisabled);
-  disabledStatus(player[1], !isDisabled);
-};
+  const ariaDisabled = () => {
+    const disabledStatus = (element, status) => {
+      status ? element.setAttribute("aria-disabled", "false") : element.setAttribute("aria-disabled", "true");
+    };
 
-const playDice = () => {
-  const dice = Math.floor(Math.random() * (6 - 1) + 1);
-  diceNumber[i].innerText = `주사위 숫자 : ${dice}`;
+    isDisabled = !isDisabled;
+    disabledStatus(player[0], isDisabled);
+    disabledStatus(player[1], !isDisabled);
+  };
 
-  if (dice < 3) {
-    diceBuild[i].innerText = `주사위 합 : ${dice}`;
-    num = 0;
-  } else {
-    diceSum = num += dice;
-    diceBuild[i].innerText = `주사위 합 : ${diceSum}`;
-  }
+  const playDice = () => {
+    const dice = Math.floor(Math.random() * (6 - 1) + 1);
+    diceNumber[i].innerText = `주사위 숫자 : ${dice}`;
 
-  holdBtn.addEventListener("click", () => {
-    totalScore = totalScore += diceSum;
-    playerScore[i].innerText = `점수 : ${totalScore}`;
-    diceSum = 0;
-
-    ariaDisabled();
-    resetDice();
-
-    if (totalScore >= 30) {
-      victory.style = "display: flex;";
-      replay.addEventListener("click", () => {
-        location.reload();
-      });
+    if (dice < 3) {
+      diceBuild[i].innerText = `주사위 합 : ${dice}`;
+      num = 0;
+    } else {
+      diceSum = num += dice;
+      diceBuild[i].innerText = `주사위 합 : ${diceSum}`;
     }
-  });
+
+    holdBtn.addEventListener("click", () => {
+      totalScore = totalScore += diceSum;
+      playerScore[i].innerText = `점수 : ${totalScore}`;
+      diceSum = 0;
+
+      ariaDisabled();
+      resetDice();
+
+      if (totalScore >= 30) {
+        victory.style = "display: flex;";
+        replay.addEventListener("click", () => {
+          location.reload();
+        });
+      }
+    });
+  };
+  playDice();
+};
+const secondPlayer = () => {
+  i = 1;
+
+  const resetDice = () => {
+    diceNumber[i].innerText = `주사위 숫자 : 0`;
+    diceBuild[i].innerText = `주사위 합 : 0`;
+    num = 0;
+    diceSum = 0;
+  };
+
+  const ariaDisabled = () => {
+    const disabledStatus = (element, status) => {
+      status ? element.setAttribute("aria-disabled", "false") : element.setAttribute("aria-disabled", "true");
+    };
+
+    isDisabled = !isDisabled;
+    disabledStatus(player[0], isDisabled);
+    disabledStatus(player[1], !isDisabled);
+  };
+
+  const playDice = () => {
+    const dice = Math.floor(Math.random() * (6 - 1) + 1);
+    diceNumber[i].innerText = `주사위 숫자 : ${dice}`;
+
+    if (dice < 3) {
+      diceBuild[i].innerText = `주사위 합 : ${dice}`;
+      num = 0;
+    } else {
+      diceSum = num += dice;
+      diceBuild[i].innerText = `주사위 합 : ${diceSum}`;
+    }
+
+    holdBtn.addEventListener("click", () => {
+      totalScore = totalScore += diceSum;
+      playerScore[i].innerText = `점수 : ${totalScore}`;
+      diceSum = 0;
+
+      ariaDisabled();
+      resetDice();
+
+      if (totalScore >= 30) {
+        victory.style = "display: flex;";
+        replay.addEventListener("click", () => {
+          location.reload();
+        });
+      }
+    });
+  };
+  playDice();
 };
 
-playBtn[i].addEventListener("click", playDice);
+playBtn[0].addEventListener("click", firstPlayer);
+playBtn[1].addEventListener("click", secondPlayer);
